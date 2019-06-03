dc3c0bad061d520fd103bb680cb6174c682c9350
mira-kovar <mira.kovar@gmail.com>
2019-06-03 12:27:02 +0200

    chore: first commit

---
+++ src/Panel.php
@@ -0,0 +1,17 @@
+<?php
+namespace PilotCz\LazyTracy;
+
+use Tracy;
+
+class Panel implements Tracy\IBarPanel
+{
+    public function getPanel()
+    {
+        // TODO: Implement getPanel() method.
+    }
+
+    public function getTab()
+    {
+        // TODO: Implement getTab() method.
+    }
+}
